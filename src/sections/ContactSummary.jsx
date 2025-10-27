1f70f9bcbe244c7b43298c57f9d4fb6cdb4204d4
kith00 <kithsandurathnayake@gmail.com>
2025-10-27 13:22:00 +0530

    let's build memorable and ispiring products and projects together section complete

---
+++ src/sections/ContactSummary.jsx
@@ -0,0 +1,61 @@
+import { useRef } from "react";
+import Marquee from "../components/Marquee";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+const ContactSummary = () => {
+  const containerRef = useRef(null);
+  const items = [
+    "Innovation",
+    "Precision",
+    "Trust",
+    "Collaboration",
+    "Excellence",
+  ];
+  const items2 = [
+    "Innovation",
+    "Precision",
+    "Trust",
+    "Collaboration",
+    "Excellence",
+  ];
+
+  useGSAP(() => {
+    gsap.to(containerRef.current, {
+      scrollTrigger: {
+        trigger: containerRef.current,
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+        markers: false,
+      },
+    });
+  }, []);
+  return (
+    <section
+      ref={containerRef}
+      className="flex flex-col items-center justify-between min-h-screen gap-12 mt-16"
+    >
+      <Marquee items={items} />
+      <div className="overflow-hidden font-light text-center contact-text-responsive">
+        <p>
+          “ Let’s build<br />
+          <span className="font-normal text-gold">memorable</span> &{" "}
+          <span className="italic">inspiring</span> <br />
+          products & projects <span className="text-gold">together</span> “
+        </p>
+      </div>
+      <Marquee
+        items={items2}
+        reverse={true}
+        className="text-black bg-transparent border-y-2"
+        iconClassName="stroke-gold stroke-2 text-primary"
+        icon="material-symbols-light:square"
+      />
+    </section>
+  );
+};
+
+export default ContactSummary;
